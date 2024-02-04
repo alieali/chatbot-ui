8d47821a4aac5f39a0ff10e3fcaa71b7b8a318dd
alieali <121768238+alieali@users.noreply.github.com>
2024-02-04 20:49:51 +0330

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,7 +50,7 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'https://vtyajsovybrqzkubilcy.supabase.co';
+  project_url TEXT := 'https://bfkljqcnmhenkqlavbkc.supabase.co';
   service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eWFqc292eWJycXprdWJpbGN5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjk3NjIxNywiZXhwIjoyMDIyNTUyMjE3fQ.hqoaIV4Ha8E7ZEiS_TXxMeCyBC4UnqO5HDNj7oCoNZo'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
